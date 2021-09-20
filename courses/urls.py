de0e6dd818108dd54b034985bc5361eb89012f00
Stefano Vivas <stefanovivas@gmail.com>
2021-09-20 11:49:30 -0300

    Add content to the module

--- courses/urls.py
+++ courses/urls.py
@@ -1,5 +1,5 @@
 
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 
@@ -8,5 +8,9 @@ urlpatterns = [
     path('create/', views.CourseCreateView.as_view(), name='course_create'),
     path('<pk>/edit/', views.CourseUpdateView.as_view(), name='update_course'),
     path('<pk>/delete/', views.CourseDeleteView.as_view(), name='delete_course'),
+    path('<pk>/module/', views.CourseModuleUpdateView.as_view(), name='course_module_update'),
+    path('module/<int:module_id>/content/<model_name>/create/', views.ContentCreateUpdateView.as_view(),name='module_content_create'),
+    path('module/<int:module_id>/content/<model_name>/<id>/', views.ContentCreateUpdateView.as_view(),name='module_content_update'),
+    path('content/<int:id>/delete/', views.ContentDeleteView.as_view(), name='module_content_delete')
 
 ]
